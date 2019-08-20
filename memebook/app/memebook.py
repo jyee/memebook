9e8894323db7e39d0d8ee414080660ecec724f6e
Jason Yee <jason.yee@datadoghq.com>
2019-08-20 11:01:45 -0700

    Added basic working guestbook

---
+++ memebook/app/memebook.py
@@ -0,0 +1,36 @@
+import os
+import redis
+
+from flask import Flask
+from flask import request, redirect, render_template, url_for
+
+# Datadog tracing and metrics
+from datadog import statsd
+
+
+redishost = os.environ.get("REDIS_HOST") or "redis-master"
+redisport = os.environ.get("REDIS_PORT") or 6379
+
+app = Flask(__name__)
+app.redis = redis.StrictRedis(host=redishost, port=redisport, db=0, charset="utf-8", decode_responses=True)
+app.redis.config_set("save", "1 1")
+
+@app.route("/", methods=["GET", "POST"])
+def main_page():
+    if request.method == "POST":
+        statsd.increment("guestbook.post")
+        app.redis.lpush("entries", request.form["entry"])
+        return redirect(url_for("main_page"))
+    else:
+        statsd.increment("guestbook.view")
+        entries = app.redis.lrange("entries", 0, -1)
+        return render_template("main.html", entries=entries)
+
+@app.route("/clear", methods=["POST"])
+def clear_entries():
+    statsd.increment("guestbook.clear")
+    app.redis.ltrim("entries", 1, 0)
+    return redirect(url_for("main_page"))
+
+if __name__ == "__main__":
+  app.run(host="0.0.0.0", port=5000)
