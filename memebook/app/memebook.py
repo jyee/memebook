b770757e1ae209c1214aa6d00534f28d115e5fa4
Jason Yee <jason.yee@datadoghq.com>
2019-08-20 15:41:22 -0700

    hide shadow traffic

--- memebook/app/memebook.py
+++ memebook/app/memebook.py
@@ -17,19 +17,21 @@ app.redis.config_set("save", "1 1")
 
 @app.route("/", methods=["GET", "POST"])
 def main_page():
+    redis_list = request.args.get("shadow") or "entries"
     if request.method == "POST":
         statsd.increment("guestbook.post")
-        app.redis.lpush("entries", request.form["entry"])
+        app.redis.lpush(redis_list, request.form["entry"])
         return redirect(url_for("main_page"))
     else:
         statsd.increment("guestbook.view")
-        entries = app.redis.lrange("entries", 0, -1)
+        entries = app.redis.lrange(redis_list, 0, -1)
         return render_template("main.html", entries=entries)
 
 @app.route("/clear", methods=["POST"])
 def clear_entries():
+    redis_list = request.args.get("shadow") or "entries"
     statsd.increment("guestbook.clear")
-    app.redis.ltrim("entries", 1, 0)
+    app.redis.ltrim(redis_list, 1, 0)
     return redirect(url_for("main_page"))
 
 if __name__ == "__main__":
