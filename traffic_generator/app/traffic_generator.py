5696b20bfbaf2b4764eaa9084972679b786af87b
Jason Yee <jason.yee@datadoghq.com>
2019-08-20 16:43:49 -0700

    pin module versions on traffic generator. update image versions

--- traffic_generator/app/traffic_generator.py
+++ traffic_generator/app/traffic_generator.py
@@ -1,6 +1,8 @@
 import os
 import random
+import string
 import time
+
 import asyncio
 from aiohttp import ClientSession
 
@@ -12,8 +14,10 @@ max_req_sleep = traffic_rate / traffic_concurrency
 
 # The url to hit
 target_url = os.environ.get("TARGET_URL") or "http://localhost:5000"
+target_url_params = os.environ.get("TARGET_URL_PARAMS") or "shadow=shadow"
 
 
+# Async function to get a url
 async def get_url(url):
     async with ClientSession() as session:
         async with session.get(url) as resp:
@@ -21,13 +25,39 @@ async def get_url(url):
             print("Get {} - status {}".format(url, resp.status))
             time.sleep(random.random() * max_req_sleep)
 
+# Async function to post to a url
+async def post_url(url, data):
+    async with ClientSession() as session:
+        async with session.post(url, data=data) as resp:
+            await resp.text()
+            print("Post {} - status {}".format(url, resp.status))
+            time.sleep(random.random() * max_req_sleep)
 
+# Helper function to generate random "phrases"
+def generate_message(length=30):
+    letters = string.ascii_lowercase
+    spaces = random.choices(range(length), k=5)
+    phrase = ""
+    for i in range(length):
+        if i in spaces:
+            phrase = phrase + " "
+        else:
+            phrase = phrase + random.choice(letters)
+    return phrase
+
+
+# Main application
 loop = asyncio.get_event_loop()
 
 while True:
     tasks = []
     for i in range(traffic_concurrency):
-        task = asyncio.ensure_future(get_url(target_url))
+        if random.random() > .33:
+            t = get_url(target_url + "?" + target_url_params)
+        else:
+            data = {'entry': generate_message()}
+            t = post_url(target_url + "?" + target_url_params, data)
+        task = asyncio.ensure_future(t)
         tasks.append(task)
     loop.run_until_complete(asyncio.wait(tasks))
 
